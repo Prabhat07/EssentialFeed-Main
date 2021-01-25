c77ad1f1f6d3fa288dbcf0a1728e4783ab824bed
Prabhat <prabhat.tiwari43@gmail.com>
2021-01-25 14:36:58 +0530

    Add Feeditem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeetItem.swift
+//  EssentialFeed
+//
+//  Created by Prabhat Tiwari on 23/01/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
--- EssentialFeed/FeetItem.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FeetItem.swift
-//  EssentialFeed
-//
-//  Created by Prabhat Tiwari on 23/01/21.
-//
-
-import Foundation
