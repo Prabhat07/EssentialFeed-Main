f9ab33d5081b34a079134566b7590ad63e675bcc
Prabhat <prabhat.tiwari43@gmail.com>
2021-01-29 11:38:51 +0530

    Remove default value.

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader {
         self.client = client
     }
     
-    public func load(comppletion: @escaping (Error) -> Void = { _ in }) {
+    public func load(comppletion: @escaping (Error) -> Void) {
         client.get(from: url) { error in
             comppletion(.connectivity)
         }
