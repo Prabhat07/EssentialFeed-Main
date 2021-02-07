95a9d5e0f865a32da9e9b23ec2aadbe46c70b524
Prabhat <prabhat.tiwari43@gmail.com>
2021-02-07 17:11:20 +0530

    Track memory leaks

--- EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/RemoteFeedLoader.swift
@@ -44,5 +44,14 @@ public final class RemoteFeedLoader {
         }
     }
     
+    private func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, response)
+            return .success(items)
+        } catch {
+            return .failuer(.invalidData)
+        }
+    }
+    
 }
 
