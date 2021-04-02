7cbb563d2439ab4495dc22bb8ecfec662c21cd6c
Prabhat <prabhat.tiwari43@gmail.com>
2021-04-02 14:03:23 +0530

    Asserting correct url and HTTP method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -28,6 +28,20 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTest: XCTestCase {
     
+    func test_geFromUrl_testCorrectUrl() {
+        URLProtocolStub.startInterceptingRquest()
+        let url = URL(string: "https//:any-url/.com")!
+        let exp = expectation(description: "Wait for request")
+        
+        URLProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        URLSessionHTTPClient().get(from: url) { _ in }
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequest()
+    }
     
     func test_getFromUrl_failsonRequestError() {
         URLProtocolStub.startInterceptingRquest()
@@ -59,6 +73,8 @@ class URLSessionHTTPClientTest: XCTestCase {
         
         private static var stubs: Stub?
         
+        private static var requestObserve: ((URLRequest) -> Void)?
+        
         private struct Stub {
             let data: Data?
             let response: URLResponse?
@@ -76,9 +92,15 @@ class URLSessionHTTPClientTest: XCTestCase {
         static func stopInterceptingRequest() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stubs = nil
+            requestObserve = nil
+        }
+        
+        static func observeRequest(observer: @escaping (URLRequest) -> Void) {
+            requestObserve = observer
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserve?(request)
             return true
         }
         
