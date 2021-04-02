ae0ff489f69d3bc5cc57e4c3ef25ab779025a1d6
Prabhat <prabhat.tiwari43@gmail.com>
2021-04-02 16:53:32 +0530

    Make factory method for system under test or SUT

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -48,7 +48,7 @@ class URLSessionHTTPClientTest: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSut().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -56,12 +56,10 @@ class URLSessionHTTPClientTest: XCTestCase {
         let url = URL(string: "https//:any-url/.com")!
         let error = NSError(domain: "Any error", code: 0)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "Wait for complition")
         
-        sut.get(from: url) { result in
+        makeSut().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
@@ -76,6 +74,11 @@ class URLSessionHTTPClientTest: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    func makeSut() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
+    
     private class URLProtocolStub: URLProtocol {
         
         private static var stubs: Stub?
