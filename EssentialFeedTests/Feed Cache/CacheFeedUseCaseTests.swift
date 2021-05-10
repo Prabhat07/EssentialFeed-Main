3047df81e3ff9dd942ffab0b1aa1f7646558e72d
Prabhat <prabhat.tiwari43@gmail.com>
2021-05-10 09:09:08 +0530

    Save method request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCacheCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -25,5 +36,28 @@ class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.deleteCacheCallCount, 0)
     }
+
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheCallCount, 1)
+    }
+    
+    
+    // MARKS: - Helpers
+    
+    private func anyUrl() -> URL {
+        return URL(string: "https//:any-url/.com")!
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageUrl: anyUrl())
+    }
+    
     
 }
