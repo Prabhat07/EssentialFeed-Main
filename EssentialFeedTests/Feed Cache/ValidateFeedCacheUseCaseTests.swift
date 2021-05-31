2169fa4fd2dda1887f4e962975f8f7736f6241e7
Prabhat <prabhat.tiwari43@gmail.com>
2021-05-31 06:40:58 +0530

    Local feed manager doesn't message upon creation (In validate cache feed case)

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  EssentialFeedTests
+//
+//  Created by Prabhat Tiwari on 31/05/21.
+//
+
+import XCTest
+import EssentialFeed
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doseNotReceivedMessageUponCereation() {
+        let (_, store) = makeSut()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARKS: - Helpers
+    
+    private func makeSut(currentDate:@escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+}
