dd2f8a3e3c5dbc48fe15ca8e7f651f79572bc272
Prabhat <prabhat.tiwari43@gmail.com>
2021-05-31 07:58:42 +0530

    Validate command dose not delete cache after SUT deallocated

--- EssentialFeed/LocalFeedLoader.swift
+++ EssentialFeed/LocalFeedLoader.swift
@@ -40,7 +40,6 @@ public final class LocalFeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
-
             switch result {
             case let .failure(error):
                 completion(.failure(error))
@@ -55,7 +54,8 @@ public final class LocalFeedLoader {
     }
 
     public func validateCache() {
-        store.retrieve { [unowned self] result in
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case .failure:
                 self.store.deleteCacheFeed { _ in }
