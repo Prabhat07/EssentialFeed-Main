e677997a0c7a98b3dd917a6c36fb81a2c434486e
Prabhat <prabhat.tiwari43@gmail.com>
2021-08-07 15:09:45 +0530

    CoreDataFeedStore.retrieve() delivers found values on non-empty cache

--- EssentialFeed/CoreDataFeedStore.swift
+++ EssentialFeed/CoreDataFeedStore.swift
@@ -20,10 +20,50 @@ public final class CoreDataFeedStore: FeedStore {
 
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        let context = self.context
+        context.perform {
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    completion(.found(
+                                feed: cache.feed
+                                    .compactMap { ($0 as? ManagedFeedImage) }
+                                    .map {
+                                        LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                                    },
+                                timeStamp: cache.timestamp))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
     
     public func insert(_ feed: [LocalFeedImage], timeStamp: Date, completion: @escaping InsertCompletion) {
+        
+        let context = self.context
+        context.perform {
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timeStamp
+                managedCache.feed = NSOrderedSet(array: feed.map { local in
+                    let managed = ManagedFeedImage(context: context)
+                    managed.id = local.id
+                    managed.imageDescription = local.description
+                    managed.location = local.location
+                    managed.url = local.url
+                    return managed
+                })
+                
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
         
     }
     
@@ -33,11 +73,13 @@ public final class CoreDataFeedStore: FeedStore {
     
 }
 
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
 
+@objc(ManagedFeedImage)
 private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
