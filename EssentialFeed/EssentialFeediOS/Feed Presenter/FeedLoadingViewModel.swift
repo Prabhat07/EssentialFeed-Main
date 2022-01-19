7780f92fb7ae78c9aa3cd7ce3cc176206166920e
Prabhat <prabhat.tiwari43@gmail.com>
2022-01-19 06:46:34 +0530

    Add viewmodel into separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presenter/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Prabhat Tiwari on 19/01/22.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presenter/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Prabhat Tiwari on 19/01/22.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
