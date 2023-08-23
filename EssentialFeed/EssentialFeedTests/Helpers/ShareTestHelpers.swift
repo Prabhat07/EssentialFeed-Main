29e689152cfff80a216ca5706fdb6ae275f5edc3
Prabhat <prabhat.tiwari43@gmail.com>
2023-08-23 23:39:27 +0530

    Moved test helpers to share scope

--- EssentialFeed/EssentialFeedTests/Helpers/ShareTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/ShareTestHelpers.swift
@@ -18,3 +18,15 @@ func anyNSError() -> NSError {
 func anyData() -> Data {
     return Data("any data".utf8)
 }
+
+
+func makeJSONData(_ jsonItems: [[String: Any]]) -> Data {
+    let itemsJson = ["items": jsonItems]
+    return try! JSONSerialization.data(withJSONObject: itemsJson)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyUrl(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
