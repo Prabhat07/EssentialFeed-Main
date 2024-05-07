a1fccb53ea696761e280db283dfb901d06430959
Prabhat <prabhat.tiwari43@gmail.com>
2024-05-07 22:30:41 +0530

    Weakify self within onRetry closure to prevent cell from holding strong references to other components

--- EssentialFeed/EssentialFeediOS/Shared UI/View/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/View/ErrorView.swift
@@ -4,26 +4,39 @@
 
 import UIKit
 
-public final class ErrorView: UIView {
-    
-    @IBOutlet private var label: UILabel!
+public final class ErrorView: UIButton {
 
     public var message: String? {
-        get { return isVisible ? label.text : nil }
+        get { return isVisible ? title(for: .normal) : nil }
         set { setMessageAnimated(newValue) }
     }
 
-    public override func awakeFromNib() {
-        super.awakeFromNib()
-
-        label.text = nil
-        alpha = 0
+    public var onHide: (() ->())?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .errorBackgroundColor
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
 
     private var isVisible: Bool {
         return alpha > 0
     }
-
+    
+    private func configure() {
+        titleLabel?.textColor = .white
+        titleLabel?.textAlignment = .center
+        titleLabel?.font = .systemFont(ofSize: 17)
+        titleLabel?.numberOfLines = 0
+        
+        addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
+        hideMessage()
+    }
+    
     private func setMessageAnimated(_ message: String?) {
         if let message = message {
             showAnimated(message)
@@ -33,19 +46,32 @@ public final class ErrorView: UIView {
     }
 
     private func showAnimated(_ message: String) {
-        label.text = message
-
+        setTitle(message, for: .normal)
+        contentEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
         UIView.animate(withDuration: 0.25) {
             self.alpha = 1
         }
     }
 
-    @IBAction private func hideMessageAnimated() {
+    @objc private func hideMessageAnimated() {
         UIView.animate(
             withDuration: 0.25,
             animations: { self.alpha = 0 },
             completion: { completed in
-                if completed { self.label.text = nil }
+                if completed { self.hideMessage() }
             })
+    }
+    
+    private func hideMessage() {
+        setTitle(nil, for: .normal)
+        alpha = 0
+        contentEdgeInsets = .init(top: -2.5, left: 0, bottom: -2.5, right: 0)
+        onHide?()
+    }
+}
+
+extension UIColor {
+    static var errorBackgroundColor: UIColor {
+        UIColor(red: 0.99951404330000004, green: 0.41759261489999999, blue: 0.4154433012, alpha: 1)
     }
 }
